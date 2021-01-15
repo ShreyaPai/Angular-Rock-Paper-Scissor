f98ff74a4a6acb29960cfb82532f936c9ed29989
Shreya Pai <ayerhs1997@gmail.com>
2021-01-15 14:06:51 +0530

    Logic to compute winner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'rock-paper-scissor';
+  constructor(private primengConfig: PrimeNGConfig) { }
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
 }
--- src/app/rock-paper-scissor/rock-paper-scissor.component.ts
+++ src/app/rock-paper-scissor/rock-paper-scissor.component.ts
@@ -8,26 +8,63 @@ import { Component, OnInit } from '@angular/core';
 export class RockPaperScissorComponent implements OnInit {
   public computerScore: number = 0;
   public playerScore: number = 0;
+  public userChoice: string;
+  public computerChoice: string;
+  public disableComputerChoice = true;
+  public count = 0;
+  public weapons = ['rock', 'paper', 'scissor'];
+  public showWinner = '';
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public pick(userSelect: string) {
+    this.count++;
+    this.userChoice = userSelect;
+    console.log('userChoice :>> ', this.userChoice);
+    setTimeout(() => {
+      const computerSelect = Math.floor(Math.random() * 3);
+      this.computerChoice = this.weapons[computerSelect]
+      console.log('computerChoice :>> ', this.computerChoice);
+    }, 500);
+      this.computeWin(this.userChoice, this.computerChoice);
+      if (this.computerScore === 5 || this.playerScore === 5) {
+        this.checkResult();
+        this.reset();
+      }
+  }
+
   computeWin(computer, player) {
-
+    switch (computer + player) {
+      case 'rockscissor':
+      case 'scrissorpaper':
+      case 'paperrock' : {
+        this.computerScore ++;
+      }
+      break;
+      case 'rockpaper':
+      case 'paperscissor':
+      case 'scissorpaper' : {
+        this.playerScore ++;
+      }
+      break;
+    }
   }
 
-  computeLose(computer, player) {
-
-  }
-
-  computeTie(computer, player) {
-
+  public checkResult() {
+    if (this.computerScore === 5) {
+      this.showWinner = 'Sorry, You Lose!'
+    } else {
+      this.showWinner = 'Yay! You Won!'
+    }
   }
 
   reset() {
     this.computerScore = 0;
     this.playerScore = 0;
+    this.userChoice = '';
+    this.computerChoice = ''
   }
 
 }
